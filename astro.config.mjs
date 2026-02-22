42d89bbfd7f2d41bfb51e805152f22768e277e04
Kapil B <kapil94fender@gmail.com>
2026-02-22 18:14:50 +0000

    Remove invalid adapter and simplify config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
-import github from '@astrojs/github';
 
 export default defineConfig({
   site: 'https://kapilbahadur.github.io',
   base: '/kapils-website/',
-  output: 'static',
-  adapter: github(),
+  output: 'static'
 });
